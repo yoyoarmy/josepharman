5ea68a435be89832ad398daa52370fdfb1068967
yoyoarmy <josepharmancollege@gmail.com>
2024-09-26 19:48:13 -0400

    Effects

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ document.getElementById('contact-form').addEventListener('submit', function(even
         alert("Please fill in all the fields");
     }
 });
+
+// JavaScript to trigger animations on scroll
+window.addEventListener('scroll', function() {
+    const projects = document.querySelectorAll('.project');
+    
+    projects.forEach(project => {
+        const rect = project.getBoundingClientRect();
+        if (rect.top < window.innerHeight) {
+            project.classList.add('fade-in');
+        }
+    });
+});
