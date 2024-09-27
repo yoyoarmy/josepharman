50eb66f947fbb0390f052e84d1154ae7873cd2b8
yoyoarmy <josepharmancollege@gmail.com>
2024-09-26 22:14:39 -0400

    Update script.js

--- trying/script.js
+++ trying/script.js
@@ -1,15 +1,4 @@
 
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent default form submission
-
-    // Simulate form submission success
-    const successMessage = document.getElementById('success-message');
-    successMessage.classList.add('visible');
-
-    // Optionally, reset the form after displaying the message
-    this.reset();
-});
-
 
 // JavaScript to trigger animations on scroll
 window.addEventListener('scroll', function() {
